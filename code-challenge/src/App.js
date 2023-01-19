1cdd613e331b6ad1af49c545e40ddabdcf24fd83
Fábio Cristóvão <fabio.ascristovao@gmail.com>
2023-01-19 17:20:10 +0000

    check server status

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [dataList, setDataList] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkStatus = async () => {
+      let res = await fetch("https://private-anon-ff1e37bcc5-blissrecruitmentapi.apiary-mock.com/health");
+      if (!res.ok) {
+        console.log(res);
+        return setError(true);
+      }
+
+      const status = await res.json();
+      setIsLoading(false);
+      console.log(status);
+    }
+    checkStatus();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!error &&
+        <h2>O servidor carregou normalme</h2>
+      }
     </div>
   );
 }
